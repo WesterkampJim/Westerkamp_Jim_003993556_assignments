06e5eb71fe2c1c65a764708f2c6e40af4059eeed
jibbodahibbo <westerkamp.j@northeastern.edu>
2026-09-17 22:51:12 -0500

    Frames and class setup
    Added Classes, variables, JPanel and JFrames.

---
+++ JavaApplication4/src/model/BankAccount.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author jtwes
+ */
+public class BankAccount {
+    String bankName;
+    double currentBalance;
+    float yearlyGrowth;
+    String pin;
+    String accountNumber;
+}
+
---
+++ JavaApplication4/src/model/Person.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author jtwes
+ */
+
+public class Person {
+
+    String FirstName;
+    String LastName;
+    int Age;
+    boolean Married;
+    
+    
+    
+    public Person(){
+        Address personAddress = new Address();
+        BankAccount personBankAccount = new BankAccount();
+    }
+
+}
+
+
+
+
